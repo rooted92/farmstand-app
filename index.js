cdcfc9aea7114b5780f7229ae6e1a11b3f190eab
Pedro Castaneda <petecast76@gmail.com>
2024-01-26 23:51:59 -0800

    feat: added products index

--- index.js
+++ index.js
@@ -18,9 +18,14 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStandApp')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World');
+app.get('/products', async (req, res) => {
+    const foundProducts = await Product.find({});
+    console.log(foundProducts);
+    // Remember that the second argument passed in to res.render is an object that contains all of the data we want to pass into the template
+    res.render('products/index.ejs', { foundProducts });
 });
+
+app.get('/products/:id')
 
 app.listen('3000', () => {
   console.log('Server started on port 3000');
