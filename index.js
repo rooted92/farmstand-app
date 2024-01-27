1dc41b631e96f62f793c7fc23796ad1bfe956e8c
Pedro Castaneda <petecast76@gmail.com>
2024-01-26 23:20:15 -0800

    feat: mongo setup

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/storeApp')
+    .then(() => {
+        console.log("Mongo Connection Open")
+    }).catch(err => {
+        console.log("Oh No Mongo Connection Error")
+        console.log(err)
+    });
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen('3000', () => {
+  console.log('Server started on port 3000');
+});
